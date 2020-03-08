4263facde9fc3b4be5717f5746272c5a3c1ec849
Angel de la Torre <angel.dlt.1996@gmail.com>
2020-03-07 20:50:04 -0800

    Got the logic for the event when player 2 joins and more than 2 players join, need to create a queue though

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,6 @@
 var currentPlayer;
 var numPlayers;
+var gameStarted = false;
 
 var config = {
   apiKey: "AIzaSyCX2ImGsjGfs3lM1z534rqg064WLNKsiIY",
@@ -14,23 +15,60 @@ firebase.initializeApp(config);
 var database = firebase.database();
 
 $(window).on("load", function() {
-  database
-    .ref()
-    .once("value")
-    .then(function(snapshot) {
-      console.log(snapshot.val());
+  database.ref().on("value", function(snapshot) {
+    console.log("Event Database");
+    // This will occur when first loading the window
+    if (numPlayers === undefined) {
+      console.log("First Initiliazing number of players");
+      numPlayers = snapshot.val().numPlayers + 1;
+      console.log("Player " + numPlayers + " has joined");
+      database.ref().set({
+        numPlayers: numPlayers
+      });
+    } else {
       numPlayers = snapshot.val().numPlayers;
-      console.log(numPlayers);
-      if (numPlayers < 2) {
-        currentPlayer = ++numPlayers;
-        console.log(numPlayers);
-        database.ref().set({
-          numPlayers: numPlayers
-        });
-      }
-    });
+    }
+
+    if (numPlayers == 2 && !gameStarted) {
+      $("#lobby-status").text("Other player has connected!!!");
+      gameStarted = true;
+    } else if (numPlayers < 2 && !gameStarted) {
+      $("#lobby-status").text("Please wait for player 2");
+    }
+    // Scenario when player 3 joins, gamestarted is first false
+    // so player 3 would no be able to start the game and this
+    // would not affect player 1 and 2
+    else if (numPlayers > 2 && !gameStarted) {
+      $("#lobby-status").text("Please wait, two players are already playing");
+    }
+  });
+  //     console.log("Entering database event");)
+  //   database.ref().on("value", function(snapshot) {
+  //     console.log("Entering database event");
+  //     numPlayers = snapshot.val().numPlayers + 1;
+
+  //     console.log("New Player has joined: " + numPlayers);
+
+  //     if (numPlayers == 2) {
+  //       $("#lobby-status").text("Other player has connected!!!");
+  //     } else if (numPlayers < 2) {
+  //       $("#lobby-status").text("Please wait for player 2");
+  //     } else {
+  //       alert("Please wait, two players are already playing");
+  //     }
+  //     currentPlayer = numPlayers;
+  //     console.log("Current Player: " + currentPlayer);
+  //   });
+
+  //RPS chosen
+  $(".choice-image").on("click", function() {
+    console.log("Choice: " + $(this).attr("id"));
+  });
+
+  //
 });
 
+// Before the user leaves reduce the number of players connected in the database
 window.addEventListener("beforeunload", function(event) {
   database.ref().set({
     numPlayers: numPlayers - 1
