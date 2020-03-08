4c2b63d94cc0325d73c12000587c78fb39595343
Angel de la Torre <angel.dlt.1996@gmail.com>
2020-03-07 17:29:56 -0800

    Added beforeunload event to keep track of number of players on site

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,38 @@
+var currentPlayer;
+var numPlayers;
+
 var config = {
-  apiKey: "AIzaSyDygud-T_9kcgIDVRk9RQhSPfLt4fHcY-Q",
-  authDomain: "test-project-9bf18.firebaseapp.com",
-  databaseURL: "https: //test-project-9bf18.firebaseio.com",
-  projectId: "test-project-9bf18",
-  storageBucket: "test-project-9bf18.appspot.com",
-  messagingSenderId: "54668172736",
-  appId: "1: 54668172736:web: 0270e016f1ce66fdecbc25"
+  apiKey: "AIzaSyCX2ImGsjGfs3lM1z534rqg064WLNKsiIY",
+  authDomain: "rock-paper-scissors-game-18b10.firebaseapp.com",
+  databaseURL: "https://rock-paper-scissors-game-18b10.firebaseio.com",
+  projectId: "rock-paper-scissors-game-18b10",
+  storageBucket: "rock-paper-scissors-game-18b10.appspot.com",
+  messagingSenderId: "453054696616",
+  appId: "1:453054696616:web:40447bdd8402fbb0b4469d"
 };
 firebase.initializeApp(config);
 var database = firebase.database();
+
+$(window).on("load", function() {
+  database
+    .ref()
+    .once("value")
+    .then(function(snapshot) {
+      console.log(snapshot.val());
+      numPlayers = snapshot.val().numPlayers;
+      console.log(numPlayers);
+      if (numPlayers < 2) {
+        currentPlayer = ++numPlayers;
+        console.log(numPlayers);
+        database.ref().set({
+          numPlayers: numPlayers
+        });
+      }
+    });
+});
+
+window.addEventListener("beforeunload", function(event) {
+  database.ref().set({
+    numPlayers: numPlayers - 1
+  });
+});
