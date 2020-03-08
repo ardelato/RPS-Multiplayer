6ab928b17c28ab03191fa5d8c512268bc5ba891b
Angel de la Torre <angel.dlt.1996@gmail.com>
2020-03-07 16:07:11 -0800

    Basic Folder Structure

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+var config = {
+  apiKey: "AIzaSyDygud-T_9kcgIDVRk9RQhSPfLt4fHcY-Q",
+  authDomain: "test-project-9bf18.firebaseapp.com",
+  databaseURL: "https: //test-project-9bf18.firebaseio.com",
+  projectId: "test-project-9bf18",
+  storageBucket: "test-project-9bf18.appspot.com",
+  messagingSenderId: "54668172736",
+  appId: "1: 54668172736:web: 0270e016f1ce66fdecbc25"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
